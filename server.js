6b61fba703df185555b98419be185ece52cb1cd6
Kacper Wiśniewski <kaka1341@gmail.com>
2020-09-04 20:01:13 +0200

    Add handlebars && create layouts

--- server.js
+++ server.js
@@ -1,48 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 const isLogged = () => {
-  return false;  
+  return true;  
 };
 
-app.use('/user', (req, res, next) => {
-  if (isLogged()) next();
+app.use('/user/:path', (req, res, next) => {
+  if (isLogged()) res.render(`user/${req.params.path}`, { layout: 'dark' });
   else res.send('You need to be logged');
 });
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index',);
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html')
+  res.render('index',);
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
-});
-
-app.get('/user/panel', (req, res) => {
-  res.show('user/panel.html');
-});
-
-app.get('/user/settings', (req, res) => {
-  res.show('user/settings.html');
+  res.render('about',);
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name : req.params.name });
+});
+
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404-not-found.html'));
+  res.status(404).render('404-not-found');
 })
 
 app.listen(8000, () => {
